0a5b39902bdd738f4a842e48625ad69079d0ac08
Jerry Krusinski <jkruzr@gmail.com>
2017-04-22 20:02:54 -0500

    added config for redux extension config

--- src/client/app/containers/App.jsx
+++ src/client/app/containers/App.jsx
@@ -7,7 +7,9 @@ import * as reducers from '../reducers';
 import Portfolio from './Portfolio.jsx';
 
 const reducer = combineReducers(reducers);
-const store = createStore(reducer);
+const store = JSON.parse(process.env.DEBUG || 'false'
+  ? createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  : createStore(reducer);
 
 class App extends Component {
   render() {
