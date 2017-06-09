82733accaf165c5e1bc13cac38955abdf6f7f67a
Jerry Krusinski <jkruzr@gmail.com>
2017-06-09 14:34:43 -0500

    coverts sendMail to use promises

--- src/server/mailHelper.js
+++ src/server/mailHelper.js
@@ -18,18 +18,24 @@ exports.sendMail = (name, email, message) => {
   email = escape(email);
   message = escape(message);
 
-  transporter.sendMail({
-    from: `"${name}" <${email}>`,
-    to: 'jkruzr@gmail.com',
-    subject: `${name} - Message From Jkrusinski.com`,
-    html: `
-      <h3>You received a message from "${name}" &lt;${email}&gt;</h3>
-      <p>${message}</p>
-    `
-  }, (err, info) => {
-    if (err) {
-      return console.log(err);
-    }
-    console.log(`Message sent by ${name}: `, info.response);
+  return new Promise((fulfill, reject) => {
+
+    transporter.sendMail(
+      {
+        from: `"${name}" <${email}>`,
+        to: 'jkruzr@gmail.com',
+        subject: `${name} - Message From Jkrusinski.com`,
+        html: `
+        <h3>You received a message from "${name}" &lt;${email}&gt;</h3>
+        <p>${message}</p>
+        `
+      },
+
+      (err, info) => {
+        if (err) { return reject(err); }
+        fulfill(info);
+      }
+    );
   });
+
 };
