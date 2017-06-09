e615a9a850bc1d57bd3d6e54680cb95e8ad227a2
Jerry Krusinski <jkruzr@gmail.com>
2017-06-09 17:26:27 -0500

    Makes MDL checkDirty in form elements when cleared programatically

--- src/client/app/components/Form.jsx
+++ src/client/app/components/Form.jsx
@@ -16,52 +16,77 @@ const styles = {
   }
 };
 
-const Form = ({ classes, form, handleName, handleEmail, handleMessage, handleForm }) => (
-  <form onSubmit={(e) => { e.preventDefault(); handleForm(form); }}>
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.checkDirty = this.checkDirty.bind(this);
+  }
 
-    <div className={`mdl-textfield mdl-js-textfield mdl-textfield--floating-label ${classes.text}`}>
-      <input
-        id="contact-name"
-        className="mdl-textfield__input"
-        type="text"
-        value={form.name}
-        onChange={(e) => handleName(e.target.value)} />
-      <label htmlFor="contact-name" className="mdl-textfield__label">Name</label>
-    </div>
+  checkDirty() {
+    console.dir(this.messageInput);
+    this.nameInput.MaterialTextfield.checkDirty();
+    this.emailInput.MaterialTextfield.checkDirty();
+    this.messageInput.MaterialTextfield.checkDirty();
+  }
 
-    <div className={`mdl-textfield mdl-js-textfield mdl-textfield--floating-label ${classes.text}`}>
-      <input
-        id="contact-email"
-        className="mdl-textfield__input"
-        type="text"
-        pattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
-        value={form.email}
-        onChange={(e) => handleEmail(e.target.value)} />
-      <label htmlFor="contact-email" className="mdl-textfield__label">Email</label>
-      <span className="mdl-textfield__error">Valid Email Required</span>
-    </div>
+  render() {
+    const { classes, form, handleName, handleEmail, handleMessage, handleForm } = this.props;
+    // make sure form clears properly with MDL when done programatically
+    setTimeout(this.checkDirty, 0);
+    return (
+      <form onSubmit={(e) => { e.preventDefault(); handleForm(form); }}>
 
-    <div className={`mdl-textfield mdl-js-textfield ${classes.text}`}>
-      <textarea
-        className="mdl-textfield__input"
-        type="text"
-        rows="3"
-        id="contact-message"
-        value={form.message}
-        onChange={(e) => handleMessage(e.target.value)}></textarea>
-      <label className="mdl-textfield__label" htmlFor="contact-message">Message</label>
-    </div>
+        <div
+          className={`mdl-textfield mdl-js-textfield mdl-textfield--floating-label ${classes.text}`}
+          ref={(textfield) => this.nameInput = textfield }>
+          <input
+            id="contact-name"
+            className="mdl-textfield__input"
+            type="text"
+            value={form.name}
+            onChange={(e) => handleName(e.target.value)} />
+          <label htmlFor="contact-name" className="mdl-textfield__label">Name</label>
+        </div>
 
-    <div className={classes.actions}>
-      <button
-        type="submit"
-        className={`mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored ${classes.button}`}>
-        Send
-      </button>
-    </div>
+        <div
+          className={`mdl-textfield mdl-js-textfield mdl-textfield--floating-label ${classes.text}`}
+          ref={(textfield) => this.emailInput = textfield}>
+          <input
+            id="contact-email"
+            className="mdl-textfield__input"
+            type="text"
+            pattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
+            value={form.email}
+            onChange={(e) => handleEmail(e.target.value)} />
+          <label htmlFor="contact-email" className="mdl-textfield__label">Email</label>
+          <span className="mdl-textfield__error">Valid Email Required</span>
+        </div>
 
-  </form>
-);
+        <div
+          className={`mdl-textfield mdl-js-textfield ${classes.text}`}
+          ref={(textfield) => this.messageInput = textfield}>
+          <textarea
+            className="mdl-textfield__input"
+            type="text"
+            rows="3"
+            id="contact-message"
+            value={form.message}
+            onChange={(e) => handleMessage(e.target.value)}></textarea>
+          <label className="mdl-textfield__label" htmlFor="contact-message">Message</label>
+        </div>
+
+        <div className={classes.actions}>
+          <button
+            type="submit"
+            className={`mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored ${classes.button}`}>
+            Send
+          </button>
+        </div>
+
+      </form>
+    );
+  }
+}
 
 Form.propTypes = {
   classes: PropTypes.object,
