1007f1c7b1d7361d80f0d0a63ff189d706e926fe
Jerry Krusinski <jkruzr@gmail.com>
2017-06-29 18:20:13 -0500

    factors out data for About.jsx

--- src/client/app/components/About.jsx
+++ src/client/app/components/About.jsx
@@ -7,6 +7,8 @@ import Image from './Image.jsx';
 import Social from './Social.jsx';
 import Project from './Project.jsx';
 
+import { aboutMe, projects, social } from '../data/about.js';
+
 const About = ({ classes }) => {
   const apiKey = 'AIzaSyB1DNpnZ23kq2IpYXYhnD2EV3Hnhbo8jXs';
   const url = `https://maps.googleapis.com/maps/api/staticmap?key=${apiKey}&center=Austin,+Texas&size=600x400&zoom=12`;
@@ -17,7 +19,7 @@ const About = ({ classes }) => {
       <div className="row">
         <div className="col-sm-7">
           <Card title="About Me" height="250px">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra lorem in erat suscipit, vitae aliquet enim auctor. Nam est eros, rutrum vel ultrices at, ultrices accumsan felis. Quisque posuere, elit eget lacinia convallis, mauris justo bibendum velit, ut tincidunt nulla tellus et lacus. Duis efficitur euismod augue, a dignissim nibh. Nulla vel ipsum elementum, vehicula ex nec, porta neque. Duis venenatis dui sit amet nisi lobortis aliquet. Nulla ullamcorper est vel rutrum facilisis. Mauris aliquet ligula non justo consequat accumsan. </p>
+            <p>{aboutMe}</p>
           </Card>
         </div>
         <div className="col-sm-5">
@@ -27,89 +29,27 @@ const About = ({ classes }) => {
 
       <Card title="Projects">
         <div className="row">
-
-          <div className="col-sm-4">
-            <Project
-              name="CardCast"
-              img="/public/media/cardcast.png"
-              github="//github.com/jkrusinski/cardcast"
-              link="//cast.jkrusinski.com">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, erat id auctor laoreet, neque metus mattis magna, quis maximus magna sapien quis ligula. Morbi at dolor tincidunt, iaculis purus a, porta nibh. Duis rutrum quam nec dapibus congue. Proin ut semper risus. Nunc suscipit lobortis ultrices. Pellentesque sed euismod lacus, et accumsan magna.
-            </Project>
-          </div>
-
-          <div className="col-sm-4">
-            <Project
-              name="My Sentry"
-              img="/public/media/mysentry.png"
-              github="//github.com/jkrusinski/ballooncat"
-              link="//ballooncat.jkrusinski.com">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, erat id auctor laoreet, neque metus mattis magna, quis maximus magna sapien quis ligula. Morbi at dolor tincidunt, iaculis purus a, porta nibh. Duis rutrum quam nec dapibus congue. Proin ut semper risus. Nunc suscipit lobortis ultrices. Pellentesque sed euismod lacus, et accumsan magna.
-            </Project>
-          </div>
-
-          <div className="col-sm-4">
-            <Project
-              name="Echoes"
-              img="/public/media/echoes.png"
-              github="//github.com/jkrusinski/ballooncat"
-              link="//ballooncat.jkrusinski.com">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, erat id auctor laoreet, neque metus mattis magna, quis maximus magna sapien quis ligula. Morbi at dolor tincidunt, iaculis purus a, porta nibh. Duis rutrum quam nec dapibus congue. Proin ut semper risus. Nunc suscipit lobortis ultrices. Pellentesque sed euismod lacus, et accumsan magna.
-            </Project>
-          </div>
-
-          <div className="col-sm-4">
-            <Project
-              name="Balloon Cat"
-              img="/public/media/ballooncat.png"
-              github="//github.com/jkrusinski/ballooncat"
-              link="//ballooncat.jkrusinski.com">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, erat id auctor laoreet, neque metus mattis magna, quis maximus magna sapien quis ligula. Morbi at dolor tincidunt, iaculis purus a, porta nibh. Duis rutrum quam nec dapibus congue. Proin ut semper risus. Nunc suscipit lobortis ultrices. Pellentesque sed euismod lacus, et accumsan magna.
-            </Project>
-          </div>
-
-          <div className="col-sm-4">
-            <Project
-              name="Dance Party"
-              img="/public/media/danceparty.png"
-              github="//github.com/jkrusinski/danceparty"
-              link="//danceparty.jkrusinski.com">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, erat id auctor laoreet, neque metus mattis magna, quis maximus magna sapien quis ligula. Morbi at dolor tincidunt, iaculis purus a, porta nibh. Duis rutrum quam nec dapibus congue. Proin ut semper risus. Nunc suscipit lobortis ultrices. Pellentesque sed euismod lacus, et accumsan magna.
-            </Project>
-          </div>
-
+          {projects.map(({name, img, github, link, description}, index) => (
+            <div className="col-sm-4" key={index}>
+              <Project name={name} img={img} github={github} link={link}>
+                {description}
+              </Project>
+            </div>
+          ))}
         </div>
       </Card>
 
       <div className="row">
-        <div className="col-sm-3">
-          <Social
-            backgroundColor="#007BB6"
-            icon="linkedin-box"
-            name="LinkedIn"
-            link="//linkedin.com/in/jkrusinski" />
-        </div>
-        <div className="col-sm-3">
-          <Social
-            backgroundColor="#171515"
-            icon="github-box"
-            name="GitHub"
-            link="//github.com/jkrusinski" />
-        </div>
-        <div className="col-sm-3">
-          <Social
-            backgroundColor="#3050A4"
-            icon="facebook-box"
-            name="Facebook"
-            link="//facebook.com/jkruzr" />
-        </div>
-        <div className="col-sm-3">
-          <Social
-            backgroundColor="#42B6E7"
-            icon="twitter-box"
-            name="Twitter"
-            link="//twitter.com/jkrusinski" />
-        </div>
+        {social.map(({backgroundColor, icon, name, link}, index) => (
+          <div className="col-sm-3" key={index}>
+            <Social
+              backgroundColor={backgroundColor}
+              icon={icon}
+              name={name}
+              link={link}
+            />
+          </div>
+        ))}
       </div>
 
     </div>
---
+++ src/client/app/data/about.js
@@ -0,0 +1,66 @@
+export const aboutMe = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra lorem in erat suscipit, vitae aliquet enim auctor. Nam est eros, rutrum vel ultrices at, ultrices accumsan felis. Quisque posuere, elit eget lacinia convallis, mauris justo bibendum velit, ut tincidunt nulla tellus et lacus. Duis efficitur euismod augue, a dignissim nibh. Nulla vel ipsum elementum, vehicula ex nec, porta neque. Duis venenatis dui sit amet nisi lobortis aliquet. Nulla ullamcorper est vel rutrum facilisis. Mauris aliquet ligula non justo consequat accumsan.';
+
+export const projects = [
+  {
+    name: 'CardCast',
+    img: '/public/media/cardcast.png',
+    github: '//github.com/jkrusinski/cardcast',
+    link: '//cast.jkrusinski.com',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, erat id auctor laoreet, neque metus mattis magna, quis maximus magna sapien quis ligula. Morbi at dolor tincidunt, iaculis purus a, porta nibh. Duis rutrum quam nec dapibus congue. Proin ut semper risus. Nunc suscipit lobortis ultrices. Pellentesque sed euismod lacus, et accumsan magna.'
+  },
+  {
+    name: 'My Sentry',
+    img: '/public/media/mysentry.png',
+    github: '//github.com/jkrusinski/my-sentry',
+    link: '#',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, erat id auctor laoreet, neque metus mattis magna, quis maximus magna sapien quis ligula. Morbi at dolor tincidunt, iaculis purus a, porta nibh. Duis rutrum quam nec dapibus congue. Proin ut semper risus. Nunc suscipit lobortis ultrices. Pellentesque sed euismod lacus, et accumsan magna.'
+  },
+  {
+    name: 'Echoes',
+    img: '/public/media/echoes.png',
+    github: '//github.com/jkrusinski/echoes',
+    link: '#',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, erat id auctor laoreet, neque metus mattis magna, quis maximus magna sapien quis ligula. Morbi at dolor tincidunt, iaculis purus a, porta nibh. Duis rutrum quam nec dapibus congue. Proin ut semper risus. Nunc suscipit lobortis ultrices. Pellentesque sed euismod lacus, et accumsan magna.'
+  },
+  {
+    name: 'Baloon Cat',
+    img: '/public/media/ballooncat.png',
+    github: '//github.com/jkrusinski/watchout',
+    link: 'ballooncat.jkrusinski.com',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, erat id auctor laoreet, neque metus mattis magna, quis maximus magna sapien quis ligula. Morbi at dolor tincidunt, iaculis purus a, porta nibh. Duis rutrum quam nec dapibus congue. Proin ut semper risus. Nunc suscipit lobortis ultrices. Pellentesque sed euismod lacus, et accumsan magna.'
+  },
+  {
+    name: 'Dance Party',
+    img: '/public/media/danceparty.png',
+    github: '//github.com/jkrusinski/adventure-time-dance-party',
+    link: 'danceparty.jkrusinski.com',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, erat id auctor laoreet, neque metus mattis magna, quis maximus magna sapien quis ligula. Morbi at dolor tincidunt, iaculis purus a, porta nibh. Duis rutrum quam nec dapibus congue. Proin ut semper risus. Nunc suscipit lobortis ultrices. Pellentesque sed euismod lacus, et accumsan magna.'
+  }
+];
+
+export const social = [
+  {
+    backgroundColor: '#007BB6',
+    icon: 'linkedin-box',
+    name: 'LinkedIn',
+    link: '//linkedin.com/in/jkrusinski'
+  },
+  {
+    backgroundColor: '#171515',
+    icon: 'github-box',
+    name: 'GitHub',
+    link: '//github.com/jkrusinski'
+  },
+  {
+    backgroundColor: '#3050A4',
+    icon: 'facebook-box',
+    name: 'Facebook',
+    link: '//facebook.com/jkruzr'
+  },
+  {
+    backgroundColor: '#42B6E7',
+    icon: 'twitter-box',
+    name: 'Twitter',
+    link: '//twitter.com/jkrusinski'
+  }
+];
